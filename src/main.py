28ecd9c7edcff752467a0b2575b6d5ec53789bc3
Charly Huang <charly.charlongo@gmail.com>
2021-01-08 09:47:09 -0500

    Implement main process in a way to align as closely as possible to its MATLAB counterpart

--- src/main.py
+++ src/main.py
@@ -2,10 +2,47 @@
 #!/usr/bin/python
 
 import rospy
+from data_importer import *
+from os.path import dirname, join, isdir
 
 if __name__=='__main__':
 	rospy.init_node('py_sfm', anonymous=True)
 
 	print('Launched py_sfm')
 
-	rospy.spin()
+	datasets_dir = join( dirname( dirname( __file__ ) ), 'penn_u_datasets' )
+	if not isdir(datasets_dir):
+		raise Exception('The provided dataset directory is not valid. Please check again.')
+	importer = Data_Importer(x1_file=join(datasets_dir, 'x1.csv'),
+							 x2_file=join(datasets_dir, 'x2.csv'),
+							 x3_file=join(datasets_dir, 'x3.csv'),
+							 C_file=join(datasets_dir, 'C.csv'),
+							 R_file=join(datasets_dir, 'R.csv'),
+							 K_file=join(datasets_dir, 'K.csv'),
+							 img1_file=join(datasets_dir, 'img1.h5'),
+							 img2_file=join(datasets_dir, 'img2.h5'),
+							 img3_file=join(datasets_dir, 'img3.h5'))
+
+	x1, x2, x3, C, R, K, img1, img2, img3 = importer.render()
+
+	# Estimate fundamental matrix
+
+	# Estimate essential matrix from fundamental matrix ( and thus we have T and R)
+
+	# Obtain 3D points using correct camera pose (Linear Triangulation)
+
+	# Find the third camera pose using Linear PnP
+
+	# Calculate reprojection point -> visualization
+
+	# Visualization
+
+	# Nonelinear triangulation
+
+	# Display point cloud and three camera poses
+
+	# Calculate reprojection points -> visualization
+
+	# Display correspondences between the keypoints and reprojection
+
+	# rospy.spin()
